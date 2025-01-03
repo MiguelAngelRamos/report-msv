5298b56a192081e8363934c05c050ce59bdaa0de
Miguel Ramos <return.infox@gmail.com>
2025-01-03 10:17:20 -0300

    dependencias necesarias para integrar el fallback

---
+++ src/main/java/com/kibernumacademy/report/beans/CircuitBreakerBean.java
@@ -0,0 +1,16 @@
+package com.kibernumacademy.report.beans;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CircuitBreakerBean {
+
+  @Bean
+  public  defaultCustomizer() {
+
+  }
+  
+}
