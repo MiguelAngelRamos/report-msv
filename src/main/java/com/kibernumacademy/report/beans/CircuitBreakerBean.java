1714fc8f415e2ff5551400769dd680f748f44a65
Miguel Ramos <return.infox@gmail.com>
2025-01-03 10:32:17 -0300

    configuracion circuit breaker

--- src/main/java/com/kibernumacademy/report/beans/CircuitBreakerBean.java
+++ src/main/java/com/kibernumacademy/report/beans/CircuitBreakerBean.java
@@ -5,12 +5,24 @@ package com.kibernumacademy.report.beans;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+
 @Configuration
 public class CircuitBreakerBean {
 
   @Bean
-  public  defaultCustomizer() {
-
+  public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+    CircuitBreakerConfig circuitbreakerConfig = CircuitBreakerConfig.custom()
+    .failureRateThreshold(50)
+    .waitDurationInOpenState(Duration.ofMillis(1000))
+    .slidingWindowSize(2)
+    .build();
+    
   }
   
 }
